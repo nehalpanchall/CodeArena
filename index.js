8f2d93e9429445deecdd9fe8aa4e77df86eb9ce2
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-27 06:22:27 -0400

    feat(middleware): enable express.urlencoded() for parsing URL-encoded data

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT ?? 3300;
 
 app.use(express.json()); // parse JSON data
 app.use(cookieParser()); // allow cookies
+app.use(express.urlencoded({ extended: true })); // parse URL encoded data
 
 app.get('/', (req, res) => {
   return res.status(200).json({ message: 'response from default slash route' });
