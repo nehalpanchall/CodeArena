95d61eabd4e829777e26b466a555b7ec5a6a3627
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-27 05:19:51 -0400

    feat(route): define default (/) route and send json response with status code using express controller

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const PORT = 600;
 
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'response from default slash route' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
