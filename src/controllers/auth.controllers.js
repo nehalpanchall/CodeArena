0d6c253872a47ffe8a6201656f937f72757bbcbd
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-27 23:53:15 -0400

    feat(auth/register): write user registration process algorithm in controller before business logic

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,12 @@
 const userRegistration = async (req, res) => {
-  return res.status(200).json({ message: 'User registration route' });
+  // 1. get the data from body
+  // 2. validate data
+  // 3. check user exist or not in db
+  // 4. if not exist, create new user in db
+  // 5. generate random token
+  // 6. store token in db
+  // 7. send token to user via email
+  // 8. return success message
 };
 
 export { userRegistration };
