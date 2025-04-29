ded8c8bb4a35bad7c2d8182bae1bad373adde799
nehalpanchall <nehalpanchal40@gmail.com>
2025-04-29 11:07:35 -0400

    feat(auth/register): check user existance in database using findUnique() method and return the json response

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,3 +1,5 @@
+import prisma from '../../prisma/index.js';
+
 const userRegistration = async (req, res) => {
   // 1. get the data from body
   const { name, email, password } = req.body;
@@ -9,12 +11,22 @@ const userRegistration = async (req, res) => {
       .json({ message: 'user credentials are required', success: false });
   }
 
-  // 3. check user exist or not in db
-  // 4. if not exist, create new user in db
-  // 5. generate random token
-  // 6. store token in db
-  // 7. send token to user via email
-  // 8. return success message
+  try {
+    // 3. check user exist or not in db
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return res
+        .status(200)
+        .json({ message: 'user already exist', success: false });
+    }
+
+    // 4. if not exist, create new user in db
+    // 5. generate random token
+    // 6. store token in db
+    // 7. send token to user via email
+    // 8. return success message
+  } catch (error) {}
 };
 
 export { userRegistration };
