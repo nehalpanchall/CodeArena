df00c1490bcb6a398e2511eb8c6898d7a3e7ef97
nehalpanchall <nehalpanchal40@gmail.com>
2025-05-02 01:12:28 -0400

    feat(middleware/isloggedin): extract the stored jwtToken from  req.cookies using optional chaining

--- src/middlewares/isLoggedIn.middleware.js
+++ src/middlewares/isLoggedIn.middleware.js
@@ -1,5 +1,7 @@
 const isLoggedIn = async (req, res) => {
   // 1. get get jwtToken from req object
+  const token = req.cookies?.jwtToken;
+
   // 2. validate jwtToken
   // 3. if valid token, decode the token
   // 4. inject the decoded token object into req object
