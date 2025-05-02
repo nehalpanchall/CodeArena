442724e624c8cddeaaa37f6cfdbcdc0e73d7a309
nehalpanchall <nehalpanchal40@gmail.com>
2025-05-02 01:09:51 -0400

    feat(middleware/isloggedin): define process algorithm of isLoggedIn middleware

--- src/middlewares/isLoggedIn.middleware.js
+++ src/middlewares/isLoggedIn.middleware.js
@@ -1,3 +1,9 @@
-const isLoggedIn = async (req, res) => {};
+const isLoggedIn = async (req, res) => {
+  // 1. get get jwtToken from req object
+  // 2. validate jwtToken
+  // 3. if valid token, decode the token
+  // 4. inject the decoded token object into req object
+  // 5. next flag
+};
 
 export default isLoggedIn;
