3e9a9c88f737d922860957058668995370beb1db
nehalpanchall <nehalpanchal40@gmail.com>
2025-05-02 01:14:16 -0400

    feat(middleware/isloggedin): validate the token and wrap code in try/catch block

--- src/middlewares/isLoggedIn.middleware.js
+++ src/middlewares/isLoggedIn.middleware.js
@@ -1,11 +1,17 @@
 const isLoggedIn = async (req, res) => {
-  // 1. get get jwtToken from req object
-  const token = req.cookies?.jwtToken;
+  try {
+    // 1. get get jwtToken from req object
+    const token = req.cookies?.jwtToken;
 
-  // 2. validate jwtToken
-  // 3. if valid token, decode the token
-  // 4. inject the decoded token object into req object
-  // 5. next flag
+    // 2. validate jwtToken
+    if (!token) {
+      return res.status(400).json({ message: 'invalid token', success: false });
+    }
+
+    // 3. if valid token, decode the token
+    // 4. inject the decoded token object into req object
+    // 5. next flag
+  } catch (error) {}
 };
 
 export default isLoggedIn;
