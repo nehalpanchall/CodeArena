e9206dc36ecb41f6234ab904e954e1659e33012d
nehalpanchall <nehalpanchal40@gmail.com>
2025-05-09 02:00:23 -0400

    feat(auth/middleware): create a middleware isAdmin to check the user is admin and define the process algorithm

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import prisma from '../../prisma/index.js';
 
 const isLoggedIn = async (req, res, next) => {
   try {
@@ -19,6 +20,14 @@ const isLoggedIn = async (req, res, next) => {
     // 5. next flag
     next();
   } catch (error) {}
+};
+
+export const isAdmin = async (req, res, next) => {
+  // 1. get the userId from req.user
+  // 2. get the user from db based on userId
+  // 3. validate the user
+  // 4. check user role is ADMIN
+  // 5. next()
 };
 
 export default isLoggedIn;
